fdca3e2d48ca5b37455fc3f8ba6f4b7d62f2a89b
Sameer-Shahzad <sameershahzad835@gmail.com>
2025-09-01 01:25:01 +0500

    fourth commit

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Category (models.Model):
+    name = models.CharField(max_length=255)
+    
+    def _str_(self):
+        return self.name
+    
+
+class Photo (models.Model):
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null = True, blank=False)
+    image = models.ImageField(null=False, blank=False)
+    description = models.CharField(max_length=255, null=False, blank=False)
+    def _str_(self):
+        return self.description
--- photos/urls.py
+++ photos/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.gallery, name='gallery'),
-    path('photo/<int:pk>/', views.viewPhoto, name="photo"),
+    path('photo/', views.viewPhoto, name="photo"),
     path('add/', views.addPhoto, name='add'),
 ]
