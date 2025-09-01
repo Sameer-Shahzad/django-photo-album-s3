616c9208675051e9190c9158735c896903d64d9f
Sameer-Shahzad <sameershahzad835@gmail.com>
2025-09-02 01:17:56 +0500

    fifth commit

--- photos/views.py
+++ photos/views.py
@@ -1,12 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from .models import Category, Photo
 
 # Create your views here.
 
 def gallery (request):
-    return render (request, 'photos/gallery.html')
+    categories = Category.objects.all()
+    photos = Photo.objects.all()
+    return render (request, 'photos/gallery.html', {'categories': categories, 'photos': photos})
 
-def viewPhoto (request):
-    return render (request, 'photos/photo.html')
+def viewPhoto (request, pk):
+    photos = Photo.objects.get(id=pk)
+    return render (request, 'photos/photo.html', {'photos': photos})
 
 def addPhoto (request):
     return render (request, 'photos/add.html')
+
