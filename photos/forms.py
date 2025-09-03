4a2f7f023d52553f7ce6178cf2d4d3b9c36c48cf
Sameer-Shahzad <sameershahzad835@gmail.com>
2025-09-03 17:11:21 +0500

    seventh commit

--- photos/forms.py
+++ photos/forms.py
@@ -5,3 +5,37 @@ class PhotoForm(forms.ModelForm):
     class Meta:
         model = Photo
         fields = ['description', 'category', 'image']
+
+
+from django import forms
+from .models import Photo, Category
+
+
+class CategoryForm(forms.ModelForm):
+    class Meta:
+        model = Category
+        fields = ['name']
+        widgets = {
+            'name': forms.TextInput(attrs={
+                "class": "shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:ring focus:border-blue-300",
+                "placeholder": "Enter new category name"
+            }),
+        }
+
+
+class PhotoForm(forms.ModelForm):
+    class Meta:
+        model = Photo
+        fields = ['description', 'category', 'image']
+        widgets = {
+            'description': forms.TextInput(attrs={
+                "placeholder": "Enter a description",
+                "class": "shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:ring focus:border-blue-300"
+            }),
+            'category': forms.Select(attrs={
+                "class": "shadow border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:ring focus:border-blue-300"
+            }),
+            'image': forms.ClearableFileInput(attrs={
+                "class": "shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:ring focus:border-blue-300"
+            }),
+        }
--- photos/views.py
+++ photos/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 
 from .models import Category, Photo
-from .forms import PhotoForm
+from .forms import PhotoForm, CategoryForm
 
 # Create your views here.
 
@@ -14,14 +14,27 @@ def viewPhoto (request, pk):
     photos = Photo.objects.get(id=pk)
     return render (request, 'photos/photo.html', {'photos': photos})
 
-def addPhoto (request):
+def addPhoto(request):
     if request.method == 'POST':
-        form = PhotoForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            return redirect('/gallery/')
-        return render(request, 'photos/add.html', {'form': form})
-    else:
-        form = PhotoForm()
-        return render (request, 'photos/add.html', {'form': form})
+        photo_form = PhotoForm(request.POST, request.FILES)
+        category_form = CategoryForm(request.POST)
 
+        if photo_form.is_valid() and category_form.is_valid():
+            category_name = category_form.cleaned_data.get('name')
+            if category_name:
+                category, created = Category.objects.get_or_create(name=category_name)
+            else:
+                category = photo_form.cleaned_data['category']
+                
+            photo = photo_form.save(commit=False)
+            photo.category = category
+            photo.save()
+            return redirect('gallery')
+    else:
+        photo_form = PhotoForm()
+        category_form = CategoryForm()
+
+    return render(request, 'photos/add.html', {
+        'form': photo_form,
+        'category_form': category_form
+    })
